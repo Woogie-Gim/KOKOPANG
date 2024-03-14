69472769a307e2735132dc68621ae937a1c6a090
김영일 <sugo2753@gmail.com>
2024-03-14 17:46:53 +0900

    Feat[Server]: refreshToken Redis에 저장

--- KOKOPangUser/src/main/java/com/koko/kokopanguser/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.koko.kokopanguser.repository;
-
-import com.koko.kokopanguser.dto.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-}
