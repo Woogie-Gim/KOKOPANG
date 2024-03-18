cdc93f741b70fbb6f4183ae9ef72feb9b4617d63
장동재 <djjang0925@gmail.com>
2024-03-18 17:57:03 +0900

    Test[Server]: WebFlux 다중 클라이언트 브로드캐스팅 테스트

--- KOKOPangMulti/src/main/java/org/koko/kokopangmulti/KokoPangMultiApplication.java
+++ KOKOPangMulti/src/main/java/org/koko/kokopangmulti/KokoPangMultiApplication.java
@@ -1,13 +1,26 @@
 package org.koko.kokopangmulti;
 
+import org.koko.kokopangmulti.TcpServer.TcpServer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KokoPangMultiApplication {
+public class KokoPangMultiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KokoPangMultiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		TcpServer tcpServer = new TcpServer(4444);
+		new Thread(() -> {
+			try {
+				tcpServer.start();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}).start();
+	}
 }
---
+++ nettyEdu2/src/main/java/com/example/nettyedu2/User.java
@@ -0,0 +1,28 @@
+package com.example.nettyedu2;
+
+import reactor.core.publisher.Mono;
+import reactor.netty.Connection;
+
+import java.util.HashMap;
+
+public class User {
+    HashMap<String, Connection> clientList = new HashMap<String, Connection>();
+
+    public synchronized void addClient(String userName, Connection connection) {
+        clientList.put(userName, connection);
+        System.out.println("[Server]" + userName + "등록됨");
+        System.out.println("[Server]" + clientList);
+//        broadcastMessage("[Server]: " + userName + "님이 입장하였습니다.");
+    }
+
+//    public synchronized void removeClient(String userName) {
+//        clientList.remove(userName);
+//        broadcastMessage("[Server]: " + userName + "님이 나갔습니다.");
+//    }
+
+    public synchronized void broadcastMessage(String message) {
+        for (Connection connection : clientList.values()) {
+            connection.outbound().sendString(Mono.just("[Server]: " + message + "\r\n")).then().subscribe();
+        }
+    }
+}
