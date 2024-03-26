0b6364a38c4de6a1a9595dee3208afdfc881d06b
장동재 <djjang0925@gmail.com>
2024-03-26 14:21:30 +0900

    Feat[TCPServer]: 방 참가 로직 추가

--- KOKOPangMulti/src/main/java/org/koko/kokopangmulti/Channel/ChannelHandler.java
+++ KOKOPangMulti/src/main/java/org/koko/kokopangmulti/Channel/ChannelHandler.java
@@ -1,5 +1,6 @@
 package org.koko.kokopangmulti.Channel;
 
+import org.koko.kokopangmulti.Object.Session;
 import reactor.netty.Connection;
 
 public class ChannelHandler {
@@ -18,12 +19,28 @@ public class ChannelHandler {
         // 동작 확인
         System.out.println("Lobby: " + ChannelList.getLobby().getSessionList());
         System.out.println(channelName + ": " + ChannelList.getChannelInfo(index).getSessionList());
+
+        // 클라이언트에게 방 인덱스 전송
+
+        // 로비 클라이언트에게 방 목록, 로비 참가 유저 변경사항 브로드 캐스팅
     }
 
-    public static void joinChannel(int roomIndex, String userName) {
+    public static void joinChannel(String userName, int roomIndex) {
+        // userName을 통해 connection 정보 추출 및 로비의 세션 목록에서 해당 유저 제거
+        Connection connection = Session.getSessionList().get(userName);
+        ChannelList.getLobby().getSessionList().remove(userName);
+
+        // roomIndex를 통해 ChannelList에서 채널 정보 파싱
         Channel channel = ChannelList.getChannelList().get(roomIndex);
-        System.out.println(channel);
-//        channel.getSessionList().put(userName);
+
+        // 해당 채널에 유저 추가
+        channel.getSessionList().put(userName, connection);
+
+        // 정상 작동 확인 테스트
+        System.out.println(channel.getChannelName() + ": " + channel.getSessionList());
+        System.out.println("Lobby: " + ChannelList.getLobby().getSessionList());
+
+        // 로비 클라이언트에게 로비 참가중인 유저 목록 브로드캐스팅
     }
 
     // 채널 내 모든 세션에 메시지 브로드캐스트
