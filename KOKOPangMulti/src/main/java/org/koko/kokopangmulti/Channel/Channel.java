8620df1fc3ceed8ea6cfa847e2f78a790eac92e3
이주현 <ljhp1004@gmail.com>
2024-03-27 14:08:21 +0900

    Fix[TcpServer]: GAME CHANNEL 생성자 수정

--- KOKOPangMulti/src/main/java/org/koko/kokopangmulti/Channel/Channel.java
+++ KOKOPangMulti/src/main/java/org/koko/kokopangmulti/Channel/Channel.java
@@ -1,19 +1,32 @@
 package org.koko.kokopangmulti.Channel;
 
+import org.koko.kokopangmulti.Object.Session;
 import reactor.netty.Connection;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 
 public class Channel {
+
     private String channelName;
     private HashMap<String, Connection> sessionList;
+    private Deque q;
 
-
+    // LOBBY CHANNEL
     public Channel(String channelName) {
         this.channelName = channelName;
         this.sessionList = new HashMap<>();
     }
 
+    // GAME CHANNEL
+    public Channel(String channelName, String userName) {
+        this.channelName = channelName;
+        this.sessionList = new HashMap<>();
+        this.sessionList.put(userName, Session.getSessionList().get(userName));
+        this.q = new ArrayDeque<String>();
+        q.addLast(userName); // 들어온 순서대로 기록
+    }
     public Channel() {
 
     }
@@ -27,6 +40,10 @@ public class Channel {
 
     public HashMap<String, Connection> getSessionList() {
         return this.sessionList;
+    }
+
+    public Deque<String> getUsers() {
+        return this.q;
     }
 
 }
