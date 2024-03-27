90c6a97e37b096d27ef52c31811572e81b45baf5
이주현 <ljhp1004@gmail.com>
2024-03-27 15:10:46 +0900

    Fix[TCPServer]: Channel Message Handling 구조 수정

--- KOKOPangMulti/src/main/java/org/koko/kokopangmulti/Channel/ChannelHandler.java
+++ KOKOPangMulti/src/main/java/org/koko/kokopangmulti/Channel/ChannelHandler.java
@@ -1,20 +1,26 @@
 package org.koko.kokopangmulti.Channel;
 
+import org.json.JSONObject;
+import org.koko.kokopangmulti.Object.Channel;
+import org.koko.kokopangmulti.Object.ChannelList;
 import org.koko.kokopangmulti.Object.Session;
 import reactor.netty.Connection;
 
 public class ChannelHandler {
     public static void createChannel(String userName, String channelName) {
+
+        System.out.println("ROOM CREATED");
+
         // userName을 통해 connection 정보 추출 및 로비의 세션 목록에서 해당 유저 제거
         Connection connection = ChannelList.getLobby().getSessionList().get(userName);
         ChannelList.getLobby().getSessionList().remove(userName);
 
         // 방 생성 및 방 목록에 방 추가 하면서 생성된 방의 index 할당
-        Channel channel = new Channel(channelName);
+        Channel channel = new Channel(channelName, userName, connection);
         int index = ChannelList.addChannel(channel);
 
-        // 생성된 방에 유저 추가
-        ChannelList.getChannelInfo(index).getSessionList().put(userName, connection);
+//        // 생성된 방에 유저 추가
+//        ChannelList.getChannelInfo(index).getSessionList().put(userName, connection);
 
         // 동작 확인
         System.out.println("Lobby: " + ChannelList.getLobby().getSessionList());
---
+++ KOKOPangMulti/src/main/java/org/koko/kokopangmulti/Channel/ChannelMsgHandler.java
@@ -0,0 +1,42 @@
+package org.koko.kokopangmulti.Channel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.koko.kokopangmulti.Object.Channel;
+import org.koko.kokopangmulti.Object.ChannelList;
+
+public class ChannelMsgHandler {
+
+    private final ChannelHandler channelHandler;
+
+    public ChannelMsgHandler(ChannelHandler channelHandler) {
+        this.channelHandler = channelHandler;
+    }
+
+    public void filterData(String userName, JSONObject data) throws JSONException {
+
+        String type = data.getString("type");
+
+        switch (type) {
+            case "create":
+                String channelName  = data.getString("channelName");
+                channelHandler.createChannel(userName, channelName);
+                break;
+            case "join":
+                roomJoined();
+                break;
+            case "leave":
+                roomLeaved();
+                break;
+        }
+    }
+
+    public void roomJoined() {
+        System.out.println("ROOM JOINED");
+    }
+
+    public void roomLeaved() {
+        System.out.println("ROOM LEAVED");
+    }
+
+}
