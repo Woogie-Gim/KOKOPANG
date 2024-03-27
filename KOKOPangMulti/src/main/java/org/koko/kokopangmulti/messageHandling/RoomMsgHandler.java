89d7e3376de4b92cd5c7d28ab505736ccd0f1f0d
이주현 <ljhp1004@gmail.com>
2024-03-27 11:54:23 +0900

    Fix[TcpServer]: Room Message Handler 분기

--- KOKOPangMulti/src/main/java/org/koko/kokopangmulti/messageHandling/RoomMsgHandler.java
+++ KOKOPangMulti/src/main/java/org/koko/kokopangmulti/messageHandling/RoomMsgHandler.java
@@ -1,31 +1,39 @@
 package org.koko.kokopangmulti.messageHandling;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.koko.kokopangmulti.Channel.ChannelHandler;
-
-import java.util.Map;
 
 public class RoomMsgHandler {
-    ObjectMapper objectMapper;
 
     public void filterData(String userName, JSONObject data) throws JSONException {
 
         String type = data.getString("type");
 
-        if (type.equals("create")) {
-            String channelName = data.getString("channelName");
-            ChannelHandler.createChannel(userName, channelName);
-        } else if (type.equals("join")) {
-            int channelIndex = data.getInt("channelIndex");
-            ChannelHandler.joinChannel(userName, channelIndex);
-        } else if (type.equals("leave")) {
-            int channelIndex = data.getInt("channelIndex");
-            ChannelHandler.leaveChannel(userName, channelIndex);
+        switch (type) {
+            case "create":
+                roomCreated();
+                break;
+            case "join":
+                roomJoined();
+                break;
+            case "leave":
+                roomLeaved();
+                break;
         }
+
+    }
+
+    public void roomCreated() {
+        System.out.println("ROOM CREATED");
+    }
+
+    public void roomJoined() {
+        System.out.println("ROOM JOINED");
+    }
+
+    public void roomLeaved() {
+        System.out.println("ROOM LEAVED");
     }
 
 }
