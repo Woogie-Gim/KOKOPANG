716cc0c738139edfe7836418f9d45835cbde56c9
이주현 <ljhp1004@gmail.com>
2024-03-28 16:50:59 +0900

    Feat[TCPServer]: 특정 channel의 session들에 broadcasting할 JSON 만들기

--- KOKOPangMulti/src/main/java/org/koko/kokopangmulti/Braodcast/ToJson.java
+++ KOKOPangMulti/src/main/java/org/koko/kokopangmulti/Braodcast/ToJson.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.koko.kokopangmulti.Object.Channel;
 import org.koko.kokopangmulti.Object.ChannelList;
+import org.koko.kokopangmulti.Object.SessionsInChannel;
 
 import java.util.*;
 
@@ -48,6 +49,44 @@ public class ToJson {
         }
 
         data.put("type", "channelList");
+        data.put("data", jsonArray);
+
+        try {
+            return objectMapper.writeValueAsString(data) + '\n';
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String channelSessionListToJSON(Channel channel) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> data = new LinkedHashMap<>();
+        List<Object> jsonArray = new ArrayList<>();
+
+        SessionsInChannel sic = channel.getSessionsInChannel();
+        List<Integer> isExisted = sic.getIsExisted();
+        int cnt = sic.getCnt();
+
+        for (int i=0; i<6; i++) {
+
+            if (isExisted.get(i)==1) {
+                Map<String,Object> temp = new LinkedHashMap<>();
+                String userName = channel.getIdxToName().get(i);
+                temp.put("userName", userName);
+                temp.put("userId", channel.getSessionList().get(userName));
+                temp.put("isReady", sic.getIsReady(i));
+                Boolean isHost = false;
+                if (i==0) { isHost = true; }
+                temp.put("isHost", isHost);
+
+                jsonArray.add(temp);
+                cnt--;
+            }
+
+            if (cnt==0) break;
+
+        }
+        data.put("type", "channelSessionList");
         data.put("data", jsonArray);
 
         try {
