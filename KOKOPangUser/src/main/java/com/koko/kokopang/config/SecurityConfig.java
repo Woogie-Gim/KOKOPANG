8257975c9cae543f02246ddb9d505c6cbd7b760b
이항우 <bullie_hu@naver.com>
2024-03-29 13:55:02 +0900

    Fix[Server] CORS 설정 변경

--- KOKOPangUser/src/main/java/com/koko/kokopang/config/SecurityConfig.java
+++ KOKOPangUser/src/main/java/com/koko/kokopang/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Collections;
 
 @Configuration
